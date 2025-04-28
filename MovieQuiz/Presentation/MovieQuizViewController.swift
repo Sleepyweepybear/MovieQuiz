da4f3105cc0a6ffb288d24fad8866a4c166d2573
sleepyweepybear <sleepingbearow@gmail.com>
2025-04-28 19:16:01 +0500

    Добавлены основные кнопки - да, нет. Добавлена UIImageView. Добавлен Вопрос, 1/10.

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -16,6 +16,30 @@ final class MovieQuizViewController: UIViewController {
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: ДА
  
+ код картинки ios THE ROOM
+ var view = UIView()
+ view.frame = CGRect(x: 0, y: 0, width: 335, height: 502)
+ let image0 = UIImage(named: "MV5BZjY0OTU1YjItZDczMi00MDkxLWE3ZDMtYWI2ZTM2ZjI4NzFmXkEyXkFqcGdeQXVyNDczOTA3ODE@._V1_FMjpg_UX900_.jpg")?.cgImage
+ let layer0 = CALayer()
+ layer0.contents = image0
+ layer0.transform = CATransform3DMakeAffineTransform(CGAffineTransform(a: 1, b: 0, c: 0, d: 1.01, tx: 0, ty: 0))
+ layer0.bounds = view.bounds
+ layer0.position = view.center
+ view.layer.addSublayer(layer0)
+
+ view.layer.cornerRadius = 20
+ view.layer.borderWidth = 8
+ view.layer.borderColor = UIColor(red: 0.376, green: 0.761, blue: 0.557, alpha: 1).cgColor
+
+ var parent = self.view!
+ parent.addSubview(view)
+ view.translatesAutoresizingMaskIntoConstraints = false
+ view.widthAnchor.constraint(equalToConstant: 335).isActive = true
+ view.heightAnchor.constraint(equalToConstant: 502).isActive = true
+ view.leadingAnchor.constraint(equalTo: parent.leadingAnchor, constant: 20).isActive = true
+ view.topAnchor.constraint(equalTo: parent.topAnchor, constant: 98).isActive = true
+
+ 
  
  Картинка: The Dark Knight
  Настоящий рейтинг: 9
