bc87e398f90a7f0b0429eb5e6a6c7c3d923e13b2
sleepyweepybear <sleepingbearow@gmail.com>
2025-04-30 16:08:32 +0500

    Добавила Custom fonts, UIImageView, label styles к Вопрос и 1/10

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -8,3 +8,21 @@ extension UIColor {
     static var ypGray: UIColor { UIColor(named: "YP Black") ?? UIColor.gray }
     static var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white }
 }
+
+extension UIFont {
+    static var ysMedium20: UIFont {
+        UIFont(name: "YSDisplay-Medium", size: 20) ?? UIFont.systemFont(ofSize: 20, weight: .medium)
+    }
+    
+    static var ysBold23: UIFont {
+        UIFont(name: "YSDisplay-Bold", size: 23) ?? UIFont.systemFont(ofSize: 23, weight: .bold)
+    }
+    
+    static var ysBold18: UIFont {
+        UIFont(name: "YSDisplay-Bold", size: 18) ?? UIFont.systemFont(ofSize: 18, weight: .bold)
+    }
+    
+    static var ysMedium16: UIFont {
+        UIFont(name: "YSDisplay-Medium", size: 16) ?? UIFont.systemFont(ofSize: 16, weight: .medium)
+    }
+}
