b176e30fc06fdd7bc21cf8462b7d1307369dd6ce
sleepyweepybear <sleepingbearow@gmail.com>
2025-06-26 16:21:32 +0500

    fixed my commit

---
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+final class AlertPresenter: AlertPresenterProtocol {
+    weak var viewController: UIViewController?
+
+    init(viewController: UIViewController) {
+        self.viewController = viewController
+    }
+
+    func present(_ model: AlertModel) {
+        let alert = UIAlertController(
+            title: model.title,
+            message: model.message,
+            preferredStyle: .alert
+        )
+
+        let action = UIAlertAction(
+            title: model.buttonText,
+            style: .default
+        ) { _ in
+            model.completion?()
+        }
+
+        alert.addAction(action)
+        viewController?.present(alert, animated: true, completion: nil)
+    }
+}
+
